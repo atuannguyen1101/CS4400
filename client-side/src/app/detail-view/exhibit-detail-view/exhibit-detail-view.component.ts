021ebf9e5daf38d507997812295d0d3b2d2b1010
Lan Le Tu <lan.letu96@gmail.com>
2018-11-16 17:55:46 -0500

    Add animal

---
+++ client-side/src/app/detail-view/exhibit-detail-view/exhibit-detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibit-detail-view',
+  templateUrl: './exhibit-detail-view.component.html',
+  styleUrls: ['./exhibit-detail-view.component.css']
+})
+export class ExhibitDetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
