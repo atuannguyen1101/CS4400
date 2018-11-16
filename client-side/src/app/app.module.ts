5698d3d372fd85d3df9d145286dfda8765a1fec7
Thinh Nguyen <tnntech@gatech.edu>
2018-11-15 19:20:49 -0500

    components created

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchDetailComponent } from './search-detail/search-detail.component';
+import { ExhibitDetailComponent } from './exhibit-detail/exhibit-detail.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { HistoryViewComponent } from './history-view/history-view.component';
+import { ShowViewComponent } from './show-view/show-view.component';
+import { AddDetailComponent } from './add-detail/add-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupComponent,
+    HomeComponent,
+    FooterComponent,
+    SearchDetailComponent,
+    ExhibitDetailComponent,
+    DetailViewComponent,
+    HistoryViewComponent,
+    ShowViewComponent,
+    AddDetailComponent
   ],
   imports: [
     BrowserModule,
