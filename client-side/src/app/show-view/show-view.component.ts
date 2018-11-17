44d31df8f4e25206a06a7241fab763b6f1e7a8a4
Lan Le Tu <lan.letu96@gmail.com>
2018-11-17 00:10:57 -0500

    Show history staff

--- client-side/src/app/show-view/show-view.component.ts
+++ client-side/src/app/show-view/show-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
 
 @Component({
   selector: 'app-show-view',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-view.component.css']
 })
 export class ShowViewComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'time', 'exhibit'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  exhibit: string;
+  time: string;
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen',  exhibit: 'H', time:'Monday'},
+  {position: 2, name: 'Helium',  exhibit: 'He',time:'Monday'},
+  {position: 3, name: 'Lithium', exhibit: 'Li', time:'Monday'},
+  {position: 4, name: 'Beryllium', exhibit: 'Be', time:'Monday'},
+  {position: 5, name: 'Beryllium2', exhibit: 'Be', time:'Monday'},
+  {position: 6, name: 'Beryllium3', exhibit: 'Be', time:'Monday'},
+  {position: 7, name: 'Beryllium4', exhibit: 'Be', time:'Monday'},
+];
+
