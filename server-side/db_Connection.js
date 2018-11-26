6d6ea96713ac2fa3ef6a0b79e2d534f4619e2a78
Thinh Nguyen <tnntech@gatech.edu>
2018-11-26 18:08:58 -0500

    update db connection

--- server-side/db_Connection.js
+++ server-side/db_Connection.js
@@ -5,5 +5,5 @@ module.exports = mysql.createConnection({
 	host: process.env.DB_HOST,
 	database: process.env.DB_TABLE,
 	user: process.env.DB_USER,
-	password:  process.env.DB_PASSWORD
+	password: process.env.DB_PASSWORD
 });
