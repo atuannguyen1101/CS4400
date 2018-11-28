f5350f0a2d3c20a8a982d2f18bdd139ba539ba57
Lan Le Tu <lan.letu96@gmail.com>
2018-11-28 10:44:49 -0500

    View and search animal view for admin

--- client-side/src/app/detail-view/view-animals-admin/view-animals-admin.component.ts
+++ client-side/src/app/detail-view/view-animals-admin/view-animals-admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
 
 @Component({
   selector: 'app-view-animals-admin',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-animals-admin.component.css']
 })
 export class ViewAnimalsAdminComponent implements OnInit {
+  displayedColumns: string[] = ['species', 'name', 'type', 'exhibit', 'age'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+  export interface PeriodicElement {
+    name: string;
+    species: string;
+    type: string;
+    age: number;
+    exhibit: string;
+
+  }
+
+  const ELEMENT_DATA: PeriodicElement[] = [
+    {age: 1, name: 'Hydrogen',  species: 'Hydrogen', type: 'Hydrogen',exhibit: 'Hydrogen'},
+    {age: 2, name: 'Helium',  species: 'Hydrogen',type: 'Hydrogen',exhibit: 'Hydrogen'}
+  ];
